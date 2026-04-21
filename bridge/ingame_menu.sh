07cb22fcca5ddb66d63078184a92bf6ae90bb214
Khazdul <olevong@gmail.com>
2026-04-22 00:29:27 +0200

    ingame_menu: update exit warning text

--- bridge/ingame_menu.sh
+++ bridge/ingame_menu.sh
@@ -362,7 +362,7 @@ _exit_confirm() {
                 printf '\n\n'
                 printf "%${pad}s${_MR_ACTIVE}%s${_MR_RESET}\n" "" "$msg"
                 printf '\n'
-                local warn="Exit terminates session!"
+                local warn="Attention! This terminates the current session."
                 local wpad=$(( (cols - ${#warn}) / 2 ))
                 [ "$wpad" -lt 0 ] && wpad=0
                 printf "%${wpad}s${_MR_ERR}%s${_MR_RESET}\n" "" "$warn"
